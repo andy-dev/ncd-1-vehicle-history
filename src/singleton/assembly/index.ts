7438052a8e9b25bea81196e2c6e9b7c85e9b369d
Andres Macedo <andres.macedo@gmail.com>
2021-10-12 05:22:54 -0500

    picked idea created basic class on singleton

--- src/singleton/assembly/index.ts
+++ src/singleton/assembly/index.ts
@@ -1,53 +1,56 @@
-import { storage, Context } from "near-sdk-core"
+import { storage, Context, PersistentMap } from "near-sdk-core";
+import { XCC_GAS, AccountId, asNEAR } from "../../utils";
+@nearBindgen
+class VehicleOwner {
+  constructor(public vehicleOwner: AccountId, public dateAcquired: string) {}
+}
+class VehicleService {
+  constructor(
+    public vehicleOwner: AccountId,
+    public dateService: string,
+    public serviceNotes: string
+  ) {}
+}
+
+const STATE = "STATE";
 
 @nearBindgen
 export class Contract {
-  private message: string = 'hello world'
-
-  // return the string 'hello world'
-  helloWorld(): string {
-    return this.message
-  }
+  constructor(
+    public playersScores: PersistentMap<
+      AccountId,
+      PlayerGuess
+    > = new PersistentMap<AccountId, PlayerGuess>("playerguess")
+  ) {}
 
   // read the given key from account (contract) storage
   read(key: string): string {
     if (isKeyInStorage(key)) {
-      return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`
+      return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`;
     } else {
-      return `🚫 Key [ ${key} ] not found in storage. ( ${this.storageReport()} )`
+      return `🚫 Key [ ${key} ] not found in storage. ( ${this.storageReport()} )`;
     }
   }
 
   // write the given value at the given key to account (contract) storage
   @mutateState()
   write(key: string, value: string): string {
-    storage.set(key, value)
-    return `✅ Data saved. ( ${this.storageReport()} )`
+    storage.set(key, value);
+    return `✅ Data saved. ( ${this.storageReport()} )`;
   }
 
   // private helper method used by read() and write() above
   private storageReport(): string {
-    return `storage [ ${Context.storageUsage} bytes ]`
+    return `storage [ ${Context.storageUsage} bytes ]`;
   }
 }
 
 /**
  * This function exists only to avoid a compiler error
  *
-
-ERROR TS2339: Property 'contains' does not exist on type 'src/singleton/assembly/index/Contract'.
-
-     return this.contains(key);
-                 ~~~~~~~~
- in ~lib/near-sdk-core/storage.ts(119,17)
-
-/Users/sherif/Documents/code/near/_projects/edu.t3/starter--near-sdk-as/node_modules/asbuild/dist/main.js:6
-        throw err;
-        ^
-
  * @param key string key in account storage
  * @returns boolean indicating whether key exists
  */
 function isKeyInStorage(key: string): bool {
-  return storage.hasKey(key)
+  return storage.hasKey(key);
 }
